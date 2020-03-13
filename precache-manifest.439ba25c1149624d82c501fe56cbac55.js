a2e9e0b56e02fcc8e22ad2597ec1c60d4d63eca7
Namra00 <namra.d.29@gmail.com>
2020-03-13 01:17:34 -0400

    Updates

---
+++ precache-manifest.439ba25c1149624d82c501fe56cbac55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0e3bddc343ebd5c92a9766df90d0592",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "22cd9cf9b62d8c379d99",
+    "url": "/monsters-rolodex/static/css/main.95dcf719.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22cd9cf9b62d8c379d99",
+    "url": "/monsters-rolodex/static/js/main.789672b6.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
